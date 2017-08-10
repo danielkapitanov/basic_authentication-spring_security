b21843a94c04696ad438b9780d01ae706ad1f99a
Daniel Kapitanov <dskapitanov@gmail.com>
2017-08-10 11:11:23 +0700

    Basic mvc spring application that uses spring security for authentication

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -68,6 +68,16 @@ public class LoginController {
 		modelAndView.setViewName("admin/home");
 		return modelAndView;
 	}
-	
+
+	@RequestMapping(value="/admin/screen", method = RequestMethod.GET)
+	public ModelAndView screen(){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+		modelAndView.setViewName("admin/screen");
+		return modelAndView;
+	}
 
 }
